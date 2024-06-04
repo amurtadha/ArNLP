e08d84ed0f388702933881b12ef69d25bf4b5fac
Murtadha Ahmed <38778295+amurtadha@users.noreply.github.com>
2024-06-04 17:45:59 +0800

    Delete download_HF.py

--- download_HF.py
+++
@@ -1,2 +0,0 @@
-from huggingface_hub import snapshot_download
-snapshot_download(repo_id="sentence-transformers/distiluse-base-multilingual-cased-v2", ignore_patterns=["*.msgpack", "*.h5", '*.safetensors'], cache_dir='workspace/plm/sbertv2')
